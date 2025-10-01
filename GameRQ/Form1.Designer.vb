b97ce6a79189013250a0fe624742439457c6ebb7
Edward.Eynon6912 <Edward.Eynon6912@BT2024-PW0HX421>
2025-10-01 08:40:35 -0400

    Add project files.

---
+++ GameRQ/Form1.Designer.vb
@@ -0,0 +1,226 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label15 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.Button10 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button11 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label15
+        '
+        Me.Label15.BackColor = System.Drawing.Color.LawnGreen
+        Me.Label15.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.Label15.Location = New System.Drawing.Point(354, 372)
+        Me.Label15.Name = "Label15"
+        Me.Label15.Size = New System.Drawing.Size(87, 51)
+        Me.Label15.TabIndex = 15
+        Me.Label15.Text = "Win"
+        Me.Label15.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.Firebrick
+        Me.Button1.ForeColor = System.Drawing.Color.Firebrick
+        Me.Button1.Location = New System.Drawing.Point(-8, -6)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(92, 474)
+        Me.Button1.TabIndex = 16
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.Firebrick
+        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button2.ForeColor = System.Drawing.Color.Transparent
+        Me.Button2.Location = New System.Drawing.Point(212, -6)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(229, 272)
+        Me.Button2.TabIndex = 17
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Button3
+        '
+        Me.Button3.BackColor = System.Drawing.Color.Firebrick
+        Me.Button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button3.ForeColor = System.Drawing.Color.Transparent
+        Me.Button3.Location = New System.Drawing.Point(119, 53)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(49, 401)
+        Me.Button3.TabIndex = 18
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'Button4
+        '
+        Me.Button4.BackColor = System.Drawing.Color.Firebrick
+        Me.Button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button4.ForeColor = System.Drawing.Color.Firebrick
+        Me.Button4.Location = New System.Drawing.Point(161, 420)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(642, 34)
+        Me.Button4.TabIndex = 19
+        Me.Button4.UseVisualStyleBackColor = False
+        '
+        'Button5
+        '
+        Me.Button5.BackColor = System.Drawing.Color.Firebrick
+        Me.Button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button5.ForeColor = System.Drawing.Color.Firebrick
+        Me.Button5.Location = New System.Drawing.Point(724, -6)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(79, 429)
+        Me.Button5.TabIndex = 20
+        Me.Button5.UseVisualStyleBackColor = False
+        '
+        'Button6
+        '
+        Me.Button6.BackColor = System.Drawing.Color.Firebrick
+        Me.Button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button6.ForeColor = System.Drawing.Color.Firebrick
+        Me.Button6.Location = New System.Drawing.Point(424, -6)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(310, 100)
+        Me.Button6.TabIndex = 21
+        Me.Button6.UseVisualStyleBackColor = False
+        '
+        'Button7
+        '
+        Me.Button7.BackColor = System.Drawing.Color.Firebrick
+        Me.Button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button7.ForeColor = System.Drawing.Color.Firebrick
+        Me.Button7.Location = New System.Drawing.Point(50, -72)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(229, 81)
+        Me.Button7.TabIndex = 22
+        Me.Button7.Text = "Button7"
+        Me.Button7.UseVisualStyleBackColor = False
+        '
+        'Button8
+        '
+        Me.Button8.BackColor = System.Drawing.Color.Firebrick
+        Me.Button8.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button8.ForeColor = System.Drawing.Color.Firebrick
+        Me.Button8.Location = New System.Drawing.Point(161, 323)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(195, 100)
+        Me.Button8.TabIndex = 23
+        Me.Button8.UseVisualStyleBackColor = False
+        '
+        'Button9
+        '
+        Me.Button9.BackColor = System.Drawing.Color.Firebrick
+        Me.Button9.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button9.ForeColor = System.Drawing.Color.Firebrick
+        Me.Button9.Location = New System.Drawing.Point(346, 323)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(229, 53)
+        Me.Button9.TabIndex = 24
+        Me.Button9.UseVisualStyleBackColor = False
+        '
+        'Button10
+        '
+        Me.Button10.BackColor = System.Drawing.Color.Firebrick
+        Me.Button10.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button10.ForeColor = System.Drawing.Color.Firebrick
+        Me.Button10.Location = New System.Drawing.Point(492, 159)
+        Me.Button10.Name = "Button10"
+        Me.Button10.Size = New System.Drawing.Size(184, 217)
+        Me.Button10.TabIndex = 25
+        Me.Button10.UseVisualStyleBackColor = False
+        '
+        'Label1
+        '
+        Me.Label1.Location = New System.Drawing.Point(-11, -6)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(838, 460)
+        Me.Label1.TabIndex = 26
+        '
+        'Button11
+        '
+        Me.Button11.Location = New System.Drawing.Point(90, 421)
+        Me.Button11.Name = "Button11"
+        Me.Button11.Size = New System.Drawing.Size(22, 23)
+        Me.Button11.TabIndex = 27
+        Me.Button11.Text = "Button11"
+        Me.Button11.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(47, 396)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(111, 16)
+        Me.Label2.TabIndex = 28
+        Me.Label2.Text = "Click here to start!"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button10)
+        Me.Controls.Add(Me.Button9)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label15)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Button11)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label15 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Button10 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button11 As Button
+    Friend WithEvents Label2 As Label
+End Class
---
+++ GameRQ/Form1.vb
@@ -0,0 +1,51 @@
+﻿Public Class Form1
+    Private Sub Button2_MouseEnter(sender As Object, e As EventArgs) Handles Button2.MouseEnter
+        Me.Close()
+    End Sub
+    Private Sub Button1_MouseEnter(sender As Object, e As EventArgs) Handles Button1.MouseEnter
+        Me.Close()
+    End Sub
+    Private Sub Button3_MouseEnter(sender As Object, e As EventArgs) Handles Button3.MouseEnter
+        Me.Close()
+    End Sub
+    Private Sub Button4_MouseEnter(sender As Object, e As EventArgs) Handles Button4.MouseEnter
+        Me.Close()
+    End Sub
+    Private Sub Button5_MouseEnter(sender As Object, e As EventArgs) Handles Button5.MouseEnter
+        Me.Close()
+    End Sub
+    Private Sub Button6_MouseEnter(sender As Object, e As EventArgs) Handles Button6.MouseEnter
+        Me.Close()
+    End Sub
+    Private Sub Button7_MouseEnter(sender As Object, e As EventArgs) Handles Button7.MouseEnter
+        Me.Close()
+    End Sub
+    Private Sub Button8_MouseEnter(sender As Object, e As EventArgs) Handles Button8.MouseEnter
+        Me.Close()
+    End Sub
+    Private Sub Button10_MouseEnter(sender As Object, e As EventArgs) Handles Button10.MouseEnter
+        Me.Close()
+    End Sub
+    Private Sub Button9_MouseEnter(sender As Object, e As EventArgs) Handles Button9.MouseEnter
+        Me.Close()
+    End Sub
+
+    Private Sub Label15_Click(sender As Object, e As EventArgs) Handles Label15.Click
+        MessageBox.Show("You win", "Good job", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+    End Sub
+
+    Private Sub Button11_Click(sender As Object, e As EventArgs) Handles Button11.Click
+        Button11.Visible = False
+        Label1.Visible = False
+        Label2.Visible = False
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Label1.Visible = True
+        Label1.BringToFront()
+        Button11.Visible = True
+        Button11.BringToFront()
+        Label2.Visible = True
+        Label2.BringToFront()
+    End Sub
+End Class
